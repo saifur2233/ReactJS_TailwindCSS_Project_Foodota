afdeaad684c6debbca4c74cc5002b55b85fbed0a
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2022-12-06 12:07:30 +0600

    Add service page added

--- src/pages/Home/HomePageServices.js
+++ src/pages/Home/HomePageServices.js
@@ -23,9 +23,12 @@ const HomePageServices = () => {
         </p>
       </div>
       <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {services.slice(0, 3).map((service) => (
-          <ServiceCard key={service._id} service={service}></ServiceCard>
-        ))}
+        {services
+          .reverse()
+          .slice(0, 3)
+          .map((service) => (
+            <ServiceCard key={service._id} service={service}></ServiceCard>
+          ))}
       </div>
       <div className="my-10 flex justify-center">
         <Link to="/services">
